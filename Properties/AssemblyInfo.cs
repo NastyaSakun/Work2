19f98aa066c94270a0ad0e22ee4dd0b0988be706
romaklayt <romakolyago@hotmail.com>
2018-09-26 01:17:49 +0300

    added new benchmarks

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
+//хах
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
